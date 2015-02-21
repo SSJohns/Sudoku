59eb6bff3ac1c46c2898f18250b4fcbe2f44c0aa
SSJohns <sjohns25@nd.edu>
2015-02-21 17:04:49 -0500

    2nd commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <vector>
 #include "puzzle.h"
+
 using namespace std;
 
 int main() {
-	
+	vector< vector<int> > truename[9];
 
 	cout << "What are the Sudoku points?" << endl;
-	cin >> 
 } 
